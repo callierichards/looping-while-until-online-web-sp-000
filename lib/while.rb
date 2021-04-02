a223f8a98fee6f48f8f8946899cea019bc1fbe97
callie rogers <callie1013@gmail.com>
2021-04-02 02:57:14 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -6,8 +6,9 @@ def using_while
     puts "Wingardium Leviosa"
     break
   end
+  levitation_force = 6
   while levitation_force <= 10
-      levitation_force = 6
+      
    puts "Wingardium Leviosa "
     levitation_force += 1
   end
