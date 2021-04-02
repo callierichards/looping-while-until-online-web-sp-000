fa3a038ae832afcc9d078482fe1730690362ecc0
callie rogers <callie1013@gmail.com>
2021-04-02 03:07:44 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -7,9 +7,8 @@ def using_while
     break
   end
   levitation_force = 6
-  while levitation_force < 10
-
-   puts "Wingardium Leviosa "
+  while levitation_force <=10
+    puts "Wingardium Leviosa"
     levitation_force += 1
   end
 end
